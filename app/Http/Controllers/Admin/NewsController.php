07f471093ef08434fe5640fff845f443d2366f47
Kholmanov Andrei Work <ankholmanov@yandex.ru>
2020-02-26 09:52:58 +0700

    lesson4

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -13,8 +13,26 @@ class NewsController extends Controller
         return view('admin.news.index', ['news' => News::$news]);
     }
 
-    public function create()
+    public function create(Request $request)
     {
+        if ($request->isMethod('post')) {
+            $request->flash();
+
+            $model = News::$news;
+            $id = count($model) + 1;
+            $model[$id]['id'] = $id;
+            $model[$id]['title'] = $request->input('title');
+            $model[$id]['text'] = $request->input('text');
+            $model[$id]['category_id'] = $request->input('category_id');
+
+            //$json = response()->json($model)->setEncodingOptions(JSON_UNESCAPED_UNICODE);
+            $json = json_encode($model);
+            $ar = json_decode($json, true);
+            dd($json, $ar, $ar[$id]);
+
+            return redirect()->route('admin.news.create');
+        }
+
         return view('admin.news.create', [
             'categories' => News::$categories
         ]);
@@ -23,7 +41,7 @@ class NewsController extends Controller
     public function update($id)
     {
         if (empty(News::$news)){
-            return redirect(route('admin.news.index'));
+            return redirect()->route('admin.news.index');
         }
 
         return view('admin.news.update', [
